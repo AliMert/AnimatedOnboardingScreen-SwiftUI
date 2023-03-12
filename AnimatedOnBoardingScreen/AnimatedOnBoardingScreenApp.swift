0e04c8bf1c7ac131aa919a630ab40b42f932123a
Ali Mert Özhayta <mozhayta@gmail.com>
2023-03-12 19:36:52 +0300

    Initial Commit

---
+++ AnimatedOnBoardingScreen/AnimatedOnBoardingScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedOnBoardingScreenApp.swift
+//  AnimatedOnBoardingScreen
+//
+//  Created by Ali Mert Özhayta on 12.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedOnBoardingScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
